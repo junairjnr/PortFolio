a7fa43661dd82da7cfd7766237137181102f92fb
Junair <junairjunu869@gmail.com>
2023-05-27 18:04:52 +0530

    first commit

---
+++ src/components/Home.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import { HiArrowNarroRifht } from 'react-icons/hi'
+
+const Home = () => {
+    return (
+        <div name="home" className='h-screen w-full bg-[#0a192f]'>
+
+            {/* container */}
+            <div className='max-w-[1000px] mx-auto px-8 flex flex-col justify-center h-full'>
+                <p>Hi, My Name Is</p>
+                <h1>Junair---34:30</h1>
+                <h2>I'm a Developer</h2>
+                <p></p>
+            </div>
+        </div>
+    )
+}
+
+export default Home
---
+++ src/components/NavBar.jsx
@@ -0,0 +1,86 @@
+import React, { useState } from 'react';
+import Logo from '../assets/logo white.png';
+import { FaBars, FaTimes, FaGithub, FaLinkedin, FaFacebook, FaWhatsapp, FaInstagram } from 'react-icons/fa';
+import { HiOutlineMai, HiOutlineMail } from 'react-icons/hi';
+import { BsFillPersonLinesFill } from 'react-icons/bs'
+
+const NavBar = () => {
+
+    const [nav, setNav] = useState(false);
+    const handleClick = () => setNav(!nav);
+    return (
+        <div className='fixed w-full h-[80px] flex justify-between items-center px-4 bg-[#0a192f] text-gray-300'>
+            <div>
+                <img src={Logo} alt='logo' className='w-16' />
+            </div>
+
+            {/* menu */}
+            <ul className='hidden md:flex'>
+                <li>Home</li>
+                <li>About</li>
+                <li>Skills</li>
+                <li>Works</li>
+                <li>Contact</li>
+            </ul>
+
+            {/* hamburger */}
+            <div
+                onClick={handleClick}
+                className='md:hidden z-10'>
+                {!nav ? <FaBars /> : <FaTimes />}
+            </div>
+
+            {/* Mobile Menu */}
+            <ul className={!nav ? 'hidden' : 'absolute top-0 left-0 w-full h-screen bg-[#0a192f] flex flex-col items-center justify-center'}>
+                <li className='py-6 text-3xl'>Home</li>
+                <li className='py-6 text-3xl'>About</li>
+                <li className='py-6 text-3xl'>Skills</li>
+                <li className='py-6 text-3xl'>Works</li>
+                <li className='py-6 text-3xl'>Contact</li>
+            </ul>
+            {/* social icons */}
+            <div className='hidden lg:flex fixed flex-col top-[35%] left-0'>
+                <ul>
+                    <li className='w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-blue-600'>
+                        <a className='flex justify-between items-center w-full text-gray-300'
+                            href='/'>
+                            LinkedIn <FaLinkedin size={30} />
+                        </a>
+                    </li>
+                    <li className='w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-[#333333]'>
+                        <a className='flex justify-between items-center w-full text-gray-300'
+                            href='/'>
+                            GitHub <FaGithub size={30} />
+                        </a>
+                    </li>
+                    <li className='w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-[#e20a0a]'>
+                        <a className='flex justify-between items-center w-full text-gray-300'
+                            href='/'>
+                            Mail <HiOutlineMail size={30} />
+                        </a>
+                    </li>
+                    {/* <li className='w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-[#19e86fc7]'>
+                        <a className='flex justify-between items-center w-full text-gray-300'
+                            href='/'>
+                            WhatsApp <FaWhatsapp size={30} />
+                        </a>
+                    </li>
+                    <li className='w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-[#d415d4cd]'>
+                        <a className='flex justify-between items-center w-full text-gray-300'
+                            href='/'>
+                            Instagram <FaInstagram size={30} />
+                        </a>
+                    </li> */}
+                    <li className='w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-[565f69]'>
+                        <a className='flex justify-between items-center w-full text-gray-300'
+                            href='/'>
+                            Resume <BsFillPersonLinesFill size={30} />
+                        </a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    )
+}
+
+export default NavBar
