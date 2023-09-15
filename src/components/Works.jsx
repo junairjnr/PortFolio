0279a8beea9d306fd6811b5d2d2d3389f9495e2d
Junair <junairjunu869@gmail.com>
2023-09-15 14:38:02 +0530

    fix

--- src/components/Works.jsx
+++ src/components/Works.jsx
@@ -22,7 +22,7 @@ const Works = () => {
             {/*Hover effect */}
             <div className="opacity-0 group-hover:opacity-100">
               <span className="text-2xl font-bold text-black tracking-wider">
-                React Js Application
+                Social Media
               </span>
               <div className="pt-8 text-center">
                 <a href="https://social-media-woad-nine.vercel.app/">
@@ -39,128 +39,6 @@ const Works = () => {
             </div>
           </div>
 
-{/*------------------------------------------------------------- */}
-          <div
-            style={{ backgroundImage: `url(${WorkImage})` }}
-            className="shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center
-           mx-auto content-div"
-          >
-            {/*Hover effect */}
-            <div className="opacity-0 group-hover:opacity-100">
-              <span className="text-2xl font-bold text-black tracking-wider">
-                React Js Application
-              </span>
-              <div className="pt-8 text-center">
-                <a href="https://social-media-woad-nine.vercel.app/">
-                  <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">
-                    Demo
-                  </button>
-                </a>
-                <a href="https://github.com/junairjnr/Social_Media">
-                  <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">
-                    Code
-                  </button>
-                </a>
-              </div>
-            </div>
-          </div>
-          <div
-            style={{ backgroundImage: `url(${WorkImage})` }}
-            className="shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center
-           mx-auto content-div"
-          >
-            {/*Hover effect */}
-            <div className="opacity-0 group-hover:opacity-100">
-              <span className="text-2xl font-bold text-black tracking-wider">
-                React Js Application
-              </span>
-              <div className="pt-8 text-center">
-                <a href="https://social-media-woad-nine.vercel.app/">
-                  <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">
-                    Demo
-                  </button>
-                </a>
-                <a href="https://github.com/junairjnr/Social_Media">
-                  <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">
-                    Code
-                  </button>
-                </a>
-              </div>
-            </div>
-          </div>
-          <div
-            style={{ backgroundImage: `url(${WorkImage})` }}
-            className="shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center
-           mx-auto content-div"
-          >
-            {/*Hover effect */}
-            <div className="opacity-0 group-hover:opacity-100">
-              <span className="text-2xl font-bold text-black tracking-wider">
-                React Js Application
-              </span>
-              <div className="pt-8 text-center">
-                <a href="https://social-media-woad-nine.vercel.app/">
-                  <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">
-                    Demo
-                  </button>
-                </a>
-                <a href="https://github.com/junairjnr/Social_Media">
-                  <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">
-                    Code
-                  </button>
-                </a>
-              </div>
-            </div>
-          </div>
-          <div
-            style={{ backgroundImage: `url(${WorkImage})` }}
-            className="shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center
-           mx-auto content-div"
-          >
-            {/*Hover effect */}
-            <div className="opacity-0 group-hover:opacity-100">
-              <span className="text-2xl font-bold text-black tracking-wider">
-                React Js Application
-              </span>
-              <div className="pt-8 text-center">
-                <a href="https://social-media-woad-nine.vercel.app/">
-                  <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">
-                    Demo
-                  </button>
-                </a>
-                <a href="https://github.com/junairjnr/Social_Media">
-                  <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">
-                    Code
-                  </button>
-                </a>
-              </div>
-            </div>
-          </div>
-          <div
-            style={{ backgroundImage: `url(${WorkImage})` }}
-            className="shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center
-           mx-auto content-div"
-          >
-            {/*Hover effect */}
-            <div className="opacity-0 group-hover:opacity-100">
-              <span className="text-2xl font-bold text-black tracking-wider">
-                React Js Application
-              </span>
-              <div className="pt-8 text-center">
-                <a href="https://social-media-woad-nine.vercel.app/">
-                  <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">
-                    Demo
-                  </button>
-                </a>
-                <a href="https://github.com/junairjnr/Social_Media">
-                  <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">
-                    Code
-                  </button>
-                </a>
-              </div>
-            </div>
-          </div>
-{/*------------------------------------------------------------- */}
 
         </div>
       </div>
